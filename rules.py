9665f1b635d14ac0ec9d1fe9761fa3285756a18c
tspurway <tspurway@gmail.com>
2014-10-28 14:13:10 -0400

    adding Q&D inferno rule to count daily fetches

--- rules.py
+++ rules.py
@@ -2,7 +2,7 @@ from inferno.lib.rule import chunk_json_stream
 from inferno.lib.rule import InfernoRule
 from inferno.lib.rule import Keyset
 from infernyx.database import insert_postgres, insert_redshift
-from infernyx.rules import impression_stats_init, parse_date, parse_ip, parse_ua, combiner
+from infernyx.rules import impression_stats_init, parse_date, parse_ip, parse_ua, combiner, count
 from functools import partial
 from config_infernyx import *
 
@@ -50,6 +50,21 @@ RULES = [
                 value_parts=['impressions', 'clicks', 'pinned', 'blocked',
                              'sponsored', 'sponsored_link', 'newtabs'],
                 table='impression_stats_daily',
+            ),
+        },
+    ),
+    InfernoRule(
+        name='count_fetches',
+        source_tags=['incoming:app'],
+        day_range=1,
+        map_input_stream=chunk_json_stream,
+        parts_preprocess=[count],
+        geoip_file=GEOIP,
+        combiner_function=combiner,
+        keysets={
+            'stats': Keyset(
+                key_parts=['date', 'ver', 'locale', 'action'],
+                value_parts=['count'],
             ),
         },
     ),
